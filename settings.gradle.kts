5f4c5aaa27affe28825ec269ccc55168ee0b6e0e
muslihudingg <47408711+muslihudingg@users.noreply.github.com>
2025-09-08 16:45:55 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GGKotlin5"
+/*include(":torka")*/
+include(":common")
+include(":barang")
+/*include(":kasirputri")*/
+/*include(":meubel")*/
+/*include(":orangtua")*/
+/*include(":pembimbing")*/
+/*include(":uploaddata")*/
+/*include(":uploaddataadmin")*/
+/*include(":pimpinan")*/
+include(":posggv2")
+/*include(":ggpos")*/
+/*include(":siswatk")*/
+/*include(":aktivasigg")*/
+/*include(":gurutk")*/
+/*include(":postint")*/
+/*include(":soonline")*/
+include(":soonline2")
+include(":model25")
+/*include(":order")*/
+include(":orderadmin")
+/*include(":mathquiz")*/
+/*include(":kasirgg")*/
+include(":gamegg")
+/*include(":bahasajepang")*/
+include(":kepegawaian")

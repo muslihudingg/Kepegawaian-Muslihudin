f3f1b5b7244f1aabf026a057ca9b11a1f5529e32
muslihudingg <47408711+muslihudingg@users.noreply.github.com>
2025-09-08 16:46:45 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,29 +14,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "GGKotlin5"
-/*include(":torka")*/
 include(":common")
-include(":barang")
-/*include(":kasirputri")*/
-/*include(":meubel")*/
-/*include(":orangtua")*/
-/*include(":pembimbing")*/
-/*include(":uploaddata")*/
-/*include(":uploaddataadmin")*/
-/*include(":pimpinan")*/
-include(":posggv2")
-/*include(":ggpos")*/
-/*include(":siswatk")*/
-/*include(":aktivasigg")*/
-/*include(":gurutk")*/
-/*include(":postint")*/
-/*include(":soonline")*/
-include(":soonline2")
-include(":model25")
-/*include(":order")*/
-include(":orderadmin")
-/*include(":mathquiz")*/
-/*include(":kasirgg")*/
-include(":gamegg")
-/*include(":bahasajepang")*/
 include(":kepegawaian")
